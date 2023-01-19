f1ca640faa5508817ffe26cb93cfe2bb4d3b6337
Biloliddin_dev <bilol9876@gmail.com>
2023-01-19 18:22:30 +0500

    first commit

---
+++ src/cards.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./cards.css";
+function Cards() {
+  <div className="cards">
+    <h1>Cards </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nihil
+      sequi sit magni aliquam voluptas dolorum amet eveniet magnam repellat.
+    </p>
+  </div>;
+}
+
+export default Cards;
